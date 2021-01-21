622a7c17e509845ca72d51ab9e5ddfad42f6f530
Anarki2600 <anarki26@gmail.com>
2021-01-22 00:10:56 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace DZ1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^[\p{L}]+$";
+            while (true)
+            {
+                Console.WriteLine("Введите Ваше имя, пожалуйста");
+                string name = Console.ReadLine();
+                if (Regex.IsMatch(name, pattern, RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine($"Привет, {name}, сегодня, {DateTime.Now.ToShortDateString()} ! ");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели некорректное имя!");
+
+                }
+            }  
+        }
+    }
+}
